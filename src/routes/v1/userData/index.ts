bd97b72292cab318e6c119938e2a38bc40344a9f
dev-sandeepsingh <sandeepsingh@ucreate.co.in>
2021-11-22 17:05:26 +0530

    Created new endpoint to update record.

--- src/routes/v1/userData/index.ts
+++ src/routes/v1/userData/index.ts
@@ -1,10 +1,12 @@
 import { FastifyInstance } from 'fastify';
 import readRoute from './read';
 import addRoute from './create';
+import updateRoute from './update';
 
 const router = async (fastify: FastifyInstance): Promise<void> => {
   await fastify.register(readRoute);
   await fastify.register(addRoute);
+  await fastify.register(updateRoute);
 };
 
 export default router;
---
+++ src/service/userData/update.ts
@@ -0,0 +1,37 @@
+import { getRepository } from 'typeorm';
+import UserDataEntity from '../../database/entity/UserData';
+import catchAsync from '../../utils/catchAsync';
+
+import logger from '../logger';
+
+const update: (
+  id: number,
+  data: Partial<UserDataEntity>,
+) => Promise<UserDataEntity> = async (id, data) =>
+  catchAsync(
+    async () => {
+      const userDataRepository = getRepository(UserDataEntity);
+      logger('debug', `service/userData/update: updating ${id} user data`);
+
+      const record = await getRepository(UserDataEntity).findOneOrFail(id);
+      const dataToSave = userDataRepository.merge(record, {
+        ...data,        
+      });
+      const updatedRecord = await userDataRepository.save(dataToSave);      
+
+      logger('debug', 'service/userData/update: user data updated');
+      return updatedRecord;
+
+    },
+    (error) => {
+      logger(
+        'error',
+        `/service/userData/update: Error ${error} when processing update for #${id}`,
+        error.stack,
+      );
+
+      throw error;
+    },
+  )();
+
+export default update;
